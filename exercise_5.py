5850fa21964c25bcad38195f0bbda64d4b0c6987
Tanya Davis <tdavis_la@outlook.com>
2025-05-20 21:54:54 -0400

    Adding exercises 5 and 6

---
+++ exercise_5.py
@@ -0,0 +1,8 @@
+var = 10
+var_type = type(var)
+
+if var_type == str:
+    print("String")
+elif var_type == int:
+    print("Integer")
+
---
+++ exercise_6.py
@@ -0,0 +1,5 @@
+d = -10
+if d < 0:
+    print("Negative")
+elif d > 0:
+    print("Positive")
